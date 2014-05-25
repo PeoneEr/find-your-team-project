26e9396f5cddfb7c39718c08357d0a4ad7a02fac
PeoneEr <peoneerko@gmail.com>
2014-05-25 11:36:23 +0700

    some code

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,17 @@
 class ProfilesController < ApplicationController
+  before_filter :authenticate_profile!, except: [:show, :index]
   inherit_resources
+
+  def edit
+    edit! {
+      unless profile_owner?
+        redirect_to profile_path(current_profile.id) and return
+      end
+    }
+  end
+
+
+  def profile_owner?
+    current_profile.id == params[:id].to_i
+  end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,7 +2,8 @@ class Profile < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessible :email, :password, :remember_me, :nickname, :skype, :avatar
+
+  attr_accessible :email, :password, :remember_me, :nickname, :skype, :avatar, :vk, :steam
 
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>"  }, :default_url => "/images/:style/missing.jpg"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
